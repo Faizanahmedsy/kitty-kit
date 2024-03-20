18efbf4e3ad54cd1f28c942fcac0072a6489ba76
faizanahmed-devstree <faizanahmed.devstree>
2024-03-20 10:19:57 +0530

    btn

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,15 +1,15 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "bg-yellow-400 text-brown-600 font-poppins font-semibold text-lg px-12 py-4 rounded-full border border-transparent transition duration-300 ease-in-out hover:text-brown-600 hover:border-yellow-500 hover:bg-yellow-500 inline-block mt-8",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -18,6 +18,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        custom:
+          "deleteBtn bg-gradient-to-r from-yellow-400 to-yellow-600 text-brown-600 font-poppins font-semibold text-lg px-12 py-4 rounded-full border border-transparent transition duration-300 ease-in-out hover:text-brown-600 hover:border-yellow-500 hover:bg-gradient-to-r hover:from-yellow-400 hover:to-yellow-600 inline-block mt-8",
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -31,26 +33,26 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
