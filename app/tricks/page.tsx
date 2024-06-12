a87b651af890366c68bdf72f5ac78520ec8234a4
faizanahmed-devstree <faizanahmed.devstree>
2024-06-12 19:39:58 +0530

    foo

--- app/tricks/page.tsx
+++ app/tricks/page.tsx
@@ -1,16 +1,9 @@
-"use client";
 import Textv2 from "@/components/module/textv2";
 
 export default function TricksPage() {
   return (
     <div className="flex  px-32  py-10  h-[calc(100vh-150px)] gap-5">
-      <div>
-        {/* <CopyBlock
-          text={props.code}
-          language={props.language}
-          showLineNumbers={props.showLineNumbers}
-        /> */}
-      </div>
+      <div></div>
       <Textv2>Hello</Textv2>
       <Textv2>Hello</Textv2>
     </div>
