4ba81e63853fe9aacae25db71f84fa98d715fcb9
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-06-16 10:27:42 +0530

    solved err

--- components/modules/helper-data-block.tsx
+++ components/modules/helper-data-block.tsx
@@ -7,28 +7,27 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { BASEURL } from "@/services/endPoints";
+import { HelpersData } from "@/lib/database";
 
-async function getData() {
-  const res = await fetch(`${BASEURL}api/helpers`);
+// async function getData() {
+//   const res = await fetch(`${BASEURL}api/helpers`);
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch helpers");
-  }
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error("Failed to fetch helpers");
+//   }
 
-  if (res.ok) {
-    // console.log(res);
-  }
+//   if (res.ok) {
+//     // console.log(res);
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
-export default async function HelpersCards() {
-  const { data } = await getData();
-
+export default function HelpersCards() {
   return (
     <>
-      {data.map((item: any, i: any) => (
+      {HelpersData.map((item: any, i: any) => (
         <Accordion type="single" collapsible key={i}>
           <AccordionItem value={"item-1"}>
             <AccordionTrigger>{item.title}</AccordionTrigger>
