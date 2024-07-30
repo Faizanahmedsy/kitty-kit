c1619e4188419c0e41d5c94efe745d4c541ec068
faizanahmed.devstree <faizanahmed.devstree@gmail.com>
2024-07-30 13:35:27 +0530

    removed links

--- components/modules/footer/footer.tsx
+++ components/modules/footer/footer.tsx
@@ -8,23 +8,11 @@ export default function Footer() {
     <footer>
       <Separator />
       <div className="flex flex-col justify-center items-center w-full font-extrabold gap-2">
-        <div className="text-md pt-6">
+        <div className="text-md p-6">
           Made with ❤️ by
           <span className="text-blue-800 ml-1">
             <Link href="https://www.devstree.ca/">Devstree IT</Link>
           </span>
-        </div>
-        <div className="pb-6 flex gap-6 text-sm">
-          <Link
-            href={"https://www.linkedin.com/company/devstreeit/mycompany/"}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <div className="flex justify-center items-center gap-1">
-              <Linkedin size={15} />
-              Linkedin
-            </div>
-          </Link>
         </div>
       </div>
     </footer>
